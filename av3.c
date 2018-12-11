e4fdddb5c709dbde0e66952501b790ef8ad807c2
izarc6 <izar.castorina@gmail.com>
2018-12-11 14:12:03 +0100

    Initial changes

--- av3.c
+++ av3.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include "my_lib.h"
+
+static struct my_stack *stack;
 
 int main(int argc, char *argv[]) {
+
+    FILE *nom_stack;
+    fgets(nom_stack, 100, stdin);
+    if (nom_stack)
+        stack = my_stack_read(nom_stack);
+    else {
+        stack = my_stack_write(nom_stack);
+    }
+
+    stack = my_stack_init(10);
 	
 }
