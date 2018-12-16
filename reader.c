cb59f059efe856daaffca3853e71be71096746de
PussyDestroyer69 <lumalisan@gmail.com>
2018-12-16 20:05:22 +0100

    I think reader is ok

--- reader.c
+++ reader.c
@@ -9,18 +9,35 @@ int main(int argc, char *argv[]) {
 	int suma = 0;
 	int min = 0;
 	int max = 0;
-	int media = 0;
+	int previo = 0;
 
 	while (contador <= my_stack_len(stack)) {
 		contador++;
 		int value = my_stack_pop(stack);
 		printf("Elemento número %d --> %d\n", contador, value);
-
-		//TODO suma
-		//TODO min
-		//TODO max
-		//TODO media
+		//Suma
+		suma =+ value;
+		//Min
+		if (contador == 1) {
+			min = value;
+		} else if (value < min) {
+			min = value;
+		}
+		//Max
+		if (contador == 1) {
+			max = value;
+		} else if (value > max) {
+			max = value;
+		}
 	}
+
+	//Media
+	int media = suma/contador;
+
+	printf("La suma es: %d\n", suma);
+	printf("El min es: %d\n", min);
+	printf("El max es: %d\n", max);
+	printf("La media es: %d\n", media);
 
 	my_stack_purge(stack);
 
